faca80a7b15d6833561f280cdabda9964d5a8651
Ibrahim Ali <19438667+ibrahimali-ca@users.noreply.github.com>
2023-12-08 19:46:30 +0300

    task component style changes, fontawesome integration

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,19 @@
 // Task.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
-const Task = ({ title, dueTime}) => {
+
+const Task = ({ title, dueTime, onComplete}) => {
     return (
-        <div className="flex items-center justify-between p02 m-2 border-b-2 border-gray-200">
-            <span className="text-lg">{title}</span>
-            <span className="text-sm">{dueTime}</span>
-            <button aria-label="Mark as complete" className="m1-2">
-            {/* Checkmark icon here */}
+       <div ClassName="bg-white shadow-lg rounded-lg p-4 mb-4">
+        <div className="flex justify-between items-center">
+            <span className="text-lg font-semibold text-gray-800">{title}</span>
+            <span className="text-sm text-gray-600">{dueTime}</span>
+            <button onClick={onComplete} aria-label="Mark as complete" className="text-green-500 hover:text-green-700">
+                <FontAwesomeIcon icon={faCheck} size="lg" />
             </button>
+       </div>
         </div>
     );
 };
