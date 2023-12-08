914ea747c9ab5c4e1a5622325813264c28250883
Ibrahim Ali <19438667+ibrahimali-ca@users.noreply.github.com>
2023-12-08 20:20:50 +0300

    added new functions to the main comp

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,63 @@
 // Main.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Category from './Category';
 
-//Sample data
-const data = {
-    mathHomework: [
-        { id: 1, title: 'Differential Equations', dueTime: 'Due in 1 day, 24 hours, and 15 minutes'},
-        { id: 2, title: 'Matrix Algebra', dueTime: 'Due in 2 days, 14 hours, and 15 minutes'}
-        // ... other tasks
-    ],
+const Main = () => {
+    // Define initial state for tasks using useState hook
+    const [tasks, setTasks] = useState({
+        mathHomework: [
+            { id: 1, title: 'Differential Equations', dueTime: 'Due in 1 day, 24 hours, and 15 minutes'},
+            { id: 2, title: 'Matrix Algebra', dueTime: 'Due in 2 days, 14 hours, and 15 minutes'}
+            // ... other tasks
+        ],
         // ...other categories
-};
+    });
 
-const Main = () => {    return (
+    // Function to add a new task (example - this will need to be integrated into your UI)
+    const addTask = (category, newTask) => {
+        setTasks({
+            ...tasks,
+            [category]: [...tasks[category], newTask]
+        });
+    };
+
+    // Function to delete a task
+    const deleteTask = (category, taskId) => {
+        setTasks({
+            ...tasks,
+            [category]: tasks[category].filter(task => task.id !== taskId)
+        });
+    };
+
+    // Function to mark a task as complete
+    // This assumes tasks have a 'completed' property that you toggle
+    const toggleTaskCompletion = (category, taskId) => {
+        setTasks({
+            ...tasks,
+            [category]: tasks[category].map(task => 
+                task.id === taskId ? { ...task, completed: !task.completed } : task
+            )
+        });
+    };
+
+    return (
         <>
-        <Header />
-        <main className="p-4">
-            <h1 className="text-4x1 font-bold mb-6">To-Do List</h1>
-            {Object.entries(data).map(([categoryName, tasks]) => (
-            <Category key={categoryName} categoryName={categoryName} tasks={tasks} />
-            ))}
-        </main>
+            <Header />
+            <main className="p-4">
+                <h1 className="text-4xl font-bold mb-6">To-Do List</h1>
+                {Object.entries(tasks).map(([categoryName, tasks]) => (
+                    <Category 
+                        key={categoryName} 
+                        categoryName={categoryName} 
+                        tasks={tasks}
+                        addTask={addTask}
+                        deleteTask={deleteTask} 
+                        toggleTaskCompletion={toggleTaskCompletion}
+                    />
+                ))}
+            </main>
         </>
     );
 };
