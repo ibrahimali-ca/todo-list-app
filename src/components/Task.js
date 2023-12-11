8f6c98b1ac4ea86e8127bc0221877047ed3bdcdb
Ibrahim Ali <19438667+ibrahimali-ca@users.noreply.github.com>
2023-12-11 09:26:09 +0300

    Update styles for completed and incomplete tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,23 +1,33 @@
 // Task.js
 import React from 'react';
+import styles from './Task.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 
-const Task = ({ id, title, dueTime, onComplete, onDelete}) => {
+const Task = ({ id, title, completed, dueTime, onComplete, onDelete}) => {
+    const taskClass = completed ? styles.completedTask : styles.incompleteTask; // Add CSS classes as needed
+
     return (
-       <div className="bg-white shadow-lg rounded-lg p-4 mb-4">
-            <div className="flex justify-between items-center">
-                <span className="text-lg font-semibold text-gray-800">{title}</span>
-                <span className="text-sm text-gray-600">{dueTime}</span>
+        <>
+            <div className={taskClass}>
+                <span>{title}</span>
+                <button onClick={() => onComplete(id)}>Complete</button>
+                {/* {Other tasks and details and delete button} */}
             </div>
-            <button onClick={() => onComplete(id)} aria-label="Mark as complete" className="text-green-500 hover:text-green-700">
-                <FontAwesomeIcon icon={faCheck} size="lg" />
-            </button>
-            <button onClick={() => onDelete(id)} aria-label="Delete task" className="text-red-500 hover:text-red-700">
-            🗑️
-            </button>
-       </div>
+        <div className="bg-white shadow-lg rounded-lg p-4 mb-4">
+                <div className="flex justify-between items-center">
+                    <span className="text-lg font-semibold text-gray-800">{title}</span>
+                    <span className="text-sm text-gray-600">{dueTime}</span>
+        </div>
+        <button onClick={() => onComplete(id)} aria-label="Mark as complete" className="text-green-500 hover:text-green-700">
+            <FontAwesomeIcon icon={faCheck} size="lg" />
+        </button>
+        <button onClick={() => onDelete(id)} aria-label="Delete task" className="text-red-500 hover:text-red-700">
+                🗑️
+        </button>
+        </div>
+    </>
     );
 };
 
